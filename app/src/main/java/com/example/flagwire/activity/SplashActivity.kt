5e45583030e75884d0313897fc194227b3d41597
AkshayVqube <akshay.vqube@gmail.com>
2020-10-07 12:44:55 +0530

    12:48

--- app/src/main/java/com/example/flagwire/activity/SplashActivity.kt
+++ app/src/main/java/com/example/flagwire/activity/SplashActivity.kt
@@ -14,6 +14,7 @@ class SplashActivity : AppCompatActivity() {
         Handler().postDelayed(Runnable {
 
             startActivity(Intent(this,LocationSelectActivity::class.java))
+            //hello there
 
         },3000)
 
